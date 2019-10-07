440f0fa38bd058d1d5900cabbc790d780a83af16
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 13:24:41 -0400

    Building Search View - Part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 /**
  * Global State of teh app
  * Search object
@@ -12,23 +13,25 @@ const state = {}
 
 const controlSearch = async () => {
     //1. Get query from view
-    const query = 'Ethiopian'; // TODO
+    const query = searchView.getInput();
 
     if (query) {
         //2. New search obj and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
