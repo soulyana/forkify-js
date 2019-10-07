808af4bbdf6a0ef5c4d2d1e434f6b942f460edb1
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 14:00:12 -0400

    rendering an AJAX loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 /**
  * Global State of teh app
  * Search object
@@ -22,11 +22,13 @@ const controlSearch = async () => {
         // 3. Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
