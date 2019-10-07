8e040e31054018951fc298e648b402157db5329c
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 16:04:33 -0400

    Building the recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -28,12 +28,19 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (error) {
+            alert('Somethng is wrong with the search...');
+            clearLoader();
+        }
+
+
     }
 }
 
@@ -54,6 +61,30 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * Recipe Controller
  */
-const r = new Recipe(47746);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe();
+            // calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+            // render recipe
+            console.log(state.recipe);
+        } catch (error) {
+            alert('Error processing recipes!');
+        }
+
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
