b20ac21c57bef99095113dc272f066c45a2da759
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 12:21:01 -0400

    making first api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 // Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = 'af202cfb67a1a63c8ea68bad9a410022';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('ethiopian');
