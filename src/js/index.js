d2902f980bbc78534975d1a96a65ff2229c2e664
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 12:43:09 -0400

    Building the Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,35 @@
 // Global app controller
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/**
+ * Global State of teh app
+ * Search object
+ * current recipe obj
+ * shopping list obj
+ * liked recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+    //1. Get query from view
+    const query = 'Ethiopian'; // TODO
+
+    if (query) {
+        //2. New search obj and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
