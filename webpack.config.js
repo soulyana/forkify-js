65c8c9790f1daa581b960f14e5da560bd8eb59c5
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 10:26:45 -0400

    a moder setup: the webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'indext.html',
+            template: './src/index.html'
+        })
+    ]
 };
