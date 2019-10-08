3977455d83bb727bf61113ebba0bab0ed5247f7e
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-07 20:30:45 -0400

    Building the shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List'; 
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -81,7 +82,6 @@ const controlRecipe = async () => {
             // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
             state.recipe.parseIngredients();
-            console.log(state.recipe.ingredients);
 
             // calculate servings and time
             state.recipe.calcTime();
@@ -113,5 +113,6 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
 });
+
+window.l = new List();
---
+++ src/js/models/List.js
@@ -0,0 +1,29 @@
+import uniqid from 'uniqid';
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(count, unit, ingredient) {
+        const item = {
+            id: uniqid(),
+            count,
+            unit,
+            ingredient
+        }
+        this.items.push(item);
+        return item;
+    }
+
+    deleteItem(id) {
+        const index = this.items.findIndex(el => el.id === id);
+        // [2,4,8] splic(1,2) => return 4,8 original array is [2]
+        // [2,4,8] slic(1,2) => return 4, original array is [2,4,8]
+        this.items.splice(index, 1);
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount;
+    }
+}
